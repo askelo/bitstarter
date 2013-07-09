5f1e63a8a57031c363e73847b2b897d42d48befb
Oleksiy Gushchyn <oleksa.gushchyn@gmail.com>
2013-07-09 23:29:40 +0000

    Reads a content of the index.html and prints it out

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require("fs");
+  var inFile = "index.html";
+  var buffer = new Buffer(fs.readFileSync(inFile));
+  var outString = buffer.toString('utf-8');
+  response.send(outString);
 });
 
 var port = process.env.PORT || 5000;
